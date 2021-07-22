6dc283ebbc7e6a8159f8f1784172fe594c003b58
xmrg <xmrgnth@gmail.com>
2021-07-22 14:43:02 +0300

    completeTask: input field and reset button

--- angular-course/course-project/src/app/app.component.ts
+++ angular-course/course-project/src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css'],
+  styles: [`
+    h1 {
+      color: lightseagreen;
+    }
+  `]
 })
 export class AppComponent {
+  title = "Angular course";
 }
--- angular-course/course-project/src/app/servers/servers.component.ts
+++ angular-course/course-project/src/app/servers/servers.component.ts
@@ -2,14 +2,44 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was created!';
+  serverName = 'TestServer';
 
-  constructor() { }
+  username = '';
+  isButtonAble = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = `Server was created! Name is ${this.serverName}`;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
+
+  checkInputText(event: any): boolean {
+    return event.target.value === '' ? this.isButtonAble = false: this.isButtonAble = true;
+  }
+
+  onClickButton(event: any) {
+    this.username = '';
+    this.isButtonAble = false;
+  }
 }
